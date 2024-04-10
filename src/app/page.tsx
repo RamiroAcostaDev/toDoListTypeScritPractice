aa19631a1dd5af0eb5dd9a856eeb2d1ad6d7e934
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2024-04-10 00:13:35 -0300

    Refactoring handleChecked function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,127 +1,232 @@
-"use client"
-import { Box, Typography} from "@mui/material";
-import {useState} from "react";
+"use client";
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
 
-export default function Home() {
+const Home = () => {
   //Types
   type Tool = {
-    id: string ,
-    name: string,
-    checked: boolean,
-    style: object,
-    date:number,
-}
+    id: string;
+    name: string;
+    checked: boolean;
+    style: object;
+    date: number;
+  };
 
-const ObjBase ={
-  id: '',
-  name: '',
-  checked: false,
-  style: {textDecoration: 'none'},
-  date: Date.now()
+  const defaultInputTool = {
+    id: "",
+    name: "",
+    checked: false,
+    style: { textDecoration: "none" },
+    date: Date.now(),
+  };
 
-}
-  
-type FormEvent = React.FormEvent<HTMLFormElement>
-type MouseEvent = React.MouseEvent<HTMLButtonElement>
-type ChangeEvent = React.ChangeEvent<HTMLInputElement>
-type SelectEvent = React.ChangeEvent<HTMLSelectElement>
+  type FormEvent = React.FormEvent<HTMLFormElement>;
+  type MouseEvent = React.MouseEvent<HTMLButtonElement>;
+  type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+  type SelectEvent = React.ChangeEvent<HTMLSelectElement>;
 
-const [toolsList, setToolsList] = useState<Tool[]>([]);
-const [InputChange, setInputCange] = useState<Tool>(ObjBase);
+  const [toolsList, setToolsList] = useState<Tool[]>([]);
+  const [InputChange, setInputChange] = useState<Tool>(defaultInputTool);
 
-  const handleInputChanges = (event:ChangeEvent)=>{
-    const newTool: Tool ={
-      id: Math.random().toString().substring(2,9),
+  const handleInputChanges = (event: ChangeEvent) => {
+    const newTool: Tool = {
+      id: Math.random().toString().substring(2, 9),
       name: event.target.value,
       checked: false,
-      style: {textDecoration: 'none'},
-      date: Date.now()
+      style: { textDecoration: "none" },
+      date: Date.now(),
     };
-    setInputCange(newTool)
-    console.log(InputChange)
-};
+    setInputChange(newTool);
+    console.log(InputChange);
+  };
 
-const addToolToList=(event: FormEvent)=>{
+  const addToolToList = (event: FormEvent) => {
     event.preventDefault();
-    
-      setToolsList([...toolsList, InputChange]);
-      setInputCange(ObjBase)
-   
-    console.log(toolsList)
-}
 
-const handleDeleteTool = (id:string )=>{
-      const newToolsList = toolsList.filter((tool)=> tool.id !== id);
-      setToolsList(newToolsList);
-}
-  
-const deleteList = ()=>{
-      setToolsList([]);
-}
+    setToolsList([...toolsList, InputChange]);
+    setInputChange(defaultInputTool);
 
-const handleCheckedTool = (event: ChangeEvent, id : string)=>{
-  const newToolsList = toolsList.map((tool)=>{
-    if (tool.id === id){
-      tool.checked = event.target.checked;
-    
-    if(tool.checked === true){
-      tool.style = {textDecoration: 'line-through'}
-    }else{
-      tool.style = {textDecoration: 'none'}
-    }
-  }
-    return tool
-  })
-  setToolsList(newToolsList);
-}
+    console.log(toolsList);
+  };
 
-const handleSortList = (event: SelectEvent)=>{
-  const sortValue = event.target.value;
-  if (sortValue === 'recent'){
-    const sortedList = toolsList.sort((a: any,b: any)=> b.date - a.date);
-    setToolsList([...sortedList]);
-  }
-  if (sortValue === 'old'){
-    const sortedList = toolsList.sort((a: any,b: any)=> a.date - b.date);
-    setToolsList([...sortedList]);
-  }
-  if (sortValue === 'AZ'){
-    const sortedList = toolsList.sort((a: any,b: any)=> a.name.localeCompare(b.name));
-    setToolsList([...sortedList]);
-  }
-  if (sortValue === 'ZA'){
-    const sortedList = toolsList.sort((a: any,b: any)=> b.name.localeCompare(a.namek));
-    setToolsList([...sortedList]);
-  }
-  if (sortValue === 'pending'){
-    const sortedList = toolsList.sort((a: any,b: any)=> a.checked - b.checked);
-    setToolsList([...sortedList]);
-  }
-  if (sortValue === 'done'){
-    const sortedList = toolsList.sort((a: any,b: any)=> b.checked - a.checked);
-    setToolsList([...sortedList]);
-  }
-  
+  const handleDeleteTool = (id: string) => {
+    const newToolsList = toolsList.filter((tool) => tool.id !== id);
+    setToolsList(newToolsList);
+  };
 
-}
+  const deleteList = () => {
+    setToolsList([]); // solucionar problema de render
+  };
 
+  const handleCheckedTool = (event: ChangeEvent, id: string) => {
+    const newToolsList = toolsList.map((tool) => {
+      if (tool.id === id) {
+        tool.checked = event.target.checked;
 
+        if (tool.checked === true) {
+          tool.style = { textDecoration: "line-through" };
+        } else {
+          tool.style = { textDecoration: "none" };
+        }
+      }
+      return tool;
+    });
+    setToolsList(newToolsList);
+  };
+
+  const handleSortList = (event: SelectEvent) => {
+    const sortValue = event.target.value;
+    const sortByType = (type: string) => {
+      let sortedList = toolsList;
+      if (type === "recent") {
+        //reducir a un switch
+        sortedList = toolsList.sort((a: Tool, b: Tool) => b.date - a.date);
+      } else if (type === "old") {
+        sortedList = toolsList.sort((a: Tool, b: Tool) => a.date - b.date);
+      } else if (type === "AZ") {
+        sortedList = toolsList.sort((a: Tool, b: Tool) =>
+          a.name.localeCompare(b.name)
+        );
+      } else if (type === "ZA") {
+        sortedList = toolsList.sort((a: Tool, b: Tool) =>
+          b.name.localeCompare(a.name)
+        );
+
+        // ver como sortear la prop checked
+        // } else if (type === "pending") {
+        //   toolsList.sort((a: Tool, b: Tool) => parseInt(a.checked) - parseInt(b.checked);
+        // } else if (type === "done") {
+        //   toolsList.sort((a: Tool, b: Tool) => b.checked - a.checked);
+        // }
+      }
+      return sortedList;
+    };
+    setToolsList([...sortByType(sortValue)]);
+  };
+  //   switch (sortValue) {
+  //     case "recent":
+  //       setToolsList([...sortByType()]);
+  //     case "old":
+  //       setToolsList([...sortByType()]);
+  //     case "AZ":
+  //       sortedList = toolsList.sort((a: any, b: any) =>
+  //         a.name.localeCompare(b.name)
+  //       );
+  //       setToolsList([...sortedList]);
+  //     case "ZA":
+  //       sortedList = toolsList.sort((a: any, b: any) =>
+  //         b.name.localeCompare(a.namek)
+  //       );
+  //       setToolsList([...sortedList]);
+  //     case "pending":
+  //       sortedList = toolsList.sort((a: any, b: any) => a.checked - b.checked);
+  //       setToolsList([...sortedList]);
+  //     case "done":
+  //       sortedList = toolsList.sort((a: any, b: any) => b.checked - a.checked);
+  //       setToolsList([...sortedList]);
+  //       break;
+  //   }
+  // };
+
+  //   if (sortValue === "recent") {
+  //     const sortedList = toolsList.sort((a: Tool, b: Tool) => b.date - a.date);
+  //     setToolsList([...sortedList]);
+  //   }
+  //   if (sortValue === "old") {
+  //     const sortedList = toolsList.sort((a: any, b: any) => a.date - b.date);
+  //     setToolsList([...sortedList]);
+  //   }
+  //   if (sortValue === "AZ") {
+  //     const sortedList = toolsList.sort((a: any, b: any) =>
+  //       a.name.localeCompare(b.name)
+  //     );
+  //     setToolsList([...sortedList]);
+  //   }
+  //   if (sortValue === "ZA") {
+  //     const sortedList = toolsList.sort((a: any, b: any) =>
+  //       b.name.localeCompare(a.namek)
+  //     );
+  //     setToolsList([...sortedList]);
+  //   }
+  //   if (sortValue === "pending") {
+  //     const sortedList = toolsList.sort(
+  //       (a: any, b: any) => a.checked - b.checked
+  //     );
+  //     setToolsList([...sortedList]);
+  //   }
+  //   if (sortValue === "done") {
+  //     const sortedList = toolsList.sort(
+  //       (a: any, b: any) => b.checked - a.checked
+  //     );
+  //     setToolsList([...sortedList]);
+  //   }
+  // };
 
   return (
-    <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} gap={5}>
-      
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      gap={5}
+    >
       <Box>
-        <Typography variant="h6" color="initial">To Do List</Typography>
+        <Typography variant="h6" color="initial">
+          To Do List
+        </Typography>
       </Box>
 
-      <Box display = {"flex"} justifyContent={'center'} alignItems={'center'} >
-        <form action="" onSubmit={addToolToList}  style={{display:'flex', gap: '10px', flexDirection:'column'}} >
-          <input value={InputChange && InputChange.name} type="text" onChange={handleInputChanges} style={{border:'1px solid black', borderRadius: '5px', padding: '3px'}}/>
-          <Box display={'flex'} justifyContent={'center'} alignItems={'center'} gap={3}>
-            <button type="submit" style={{border:'1px solid black', borderRadius: '5px', padding: '3px'}}>Add</button>
-            <button onClick={deleteList} style={{border:'1px solid black', borderRadius: '5px', padding: '3px'}}>Delete all</button>
-            <select id="sortoptions" onChange={handleSortList} style={{border:'1px solid black', borderRadius: '5px', padding: '3px'}}>
-              
+      <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+        <form
+          action=""
+          onSubmit={addToolToList}
+          style={{ display: "flex", gap: "10px", flexDirection: "column" }}
+        >
+          <input
+            value={InputChange.name}
+            type="text"
+            onChange={handleInputChanges}
+            style={{
+              border: "1px solid black",
+              borderRadius: "5px",
+              padding: "3px",
+            }}
+          />
+          <Box
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={3}
+          >
+            <button
+              type="submit"
+              style={{
+                border: "1px solid black",
+                borderRadius: "5px",
+                padding: "3px",
+              }}
+            >
+              Add
+            </button>
+            <button
+              onClick={deleteList}
+              style={{
+                border: "1px solid black",
+                borderRadius: "5px",
+                padding: "3px",
+              }}
+            >
+              Delete all
+            </button>
+            <select
+              id="sortoptions"
+              onChange={handleSortList}
+              style={{
+                border: "1px solid black",
+                borderRadius: "5px",
+                padding: "3px",
+              }}
+            >
               <option value="old">Mas antiguas</option>
 
               <option value="recent">Mas recientes</option>
@@ -133,32 +238,46 @@ const handleSortList = (event: SelectEvent)=>{
               <option value="pending">Tareas pendientes</option>
 
               <option value="done">Tareas completadas</option>
-
             </select>
           </Box>
-
         </form>
       </Box>
 
-      <Box display = {"flex"}>
-        <ul style={{display:'flex', gap: '10px', flexDirection:'column'}}>
-          {toolsList && toolsList.map(({id, name, checked, style}, index)=>(
-          
-            
-            <li key={index} style={{display: 'flex', justifyContent:'center', alignItems: 'center', gap: '10px'}}>
-              {id && <input type='checkbox' checked={checked} onChange={(event)=>handleCheckedTool(event, id)}/>}
-              <p style={style}>{name}</p>
-             
-             {id && <button onClick={()=>handleDeleteTool(id)} style={{border:'1px solid black', borderRadius: '5px', padding: '3px'}}>Delete</button>}
+      <Box display={"flex"}>
+        <ul style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+          {toolsList &&
+            toolsList.map(({ id, name, checked, style }, index) => (
+              <li
+                key={index}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+              >
+                <input
+                  type="checkbox"
+                  checked={checked}
+                  onChange={(event) => handleCheckedTool(event, id)}
+                />
+                <p style={style}>{name}</p>
+                <button
+                  onClick={() => handleDeleteTool(id)}
+                  style={{
+                    border: "1px solid black",
+                    borderRadius: "5px",
+                    padding: "3px",
+                  }}
+                >
+                  Delete
+                </button>
               </li>
-            
-          
-          ))}
+            ))}
         </ul>
-          
       </Box>
-
     </Box>
-   
   );
-}
+};
+
+export default Home;
